679cfb00f8148d77f3de76d2e454133b89c39182
unknown <zaaferkenza08@gmail.com>
2023-03-31 15:48:51 +0000

    About us

--- petplanet/src/components/AboutComponent.vue
+++ petplanet/src/components/AboutComponent.vue
@@ -1,7 +1,25 @@
 <template>
     <div>
        <header-component></header-component>
-           
+           <div class="w-full flex flex-col sm:flex-row justify-between items-center text-justify">
+               <div class="flex flex-col justify-between items-center sm:w-1/2 w-full"> 
+                 <h5 class="font-extrabold text-4xl sm:mx-10 sm:my-6 my-2 mx-4">Welcome to PetPlanet, a trusted online destination for all pet lovers!</h5>
+                 <p class="text-sm px-8 text-justify">At PetPlanet, we understand the joy and companionship that pets bring into our lives. That's why we are dedicated to providing pet owners with the best products, resources, and information to help them care for their furry, feathered, or scaled companions.</p>
+                 <img src="../assets/image13.png" class="sm:block hidden"/>
+               </div>
+               <div class="text-justify flex flex-col justify-center items-center sm:w-1/2 w-full"> 
+                 <h5 class="font-extrabold text-2xl mx-10 sm:my-6 my-2">Waiting for you to contact us</h5>
+                 <ul>
+                    <li class="flex text-justify"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M9.20711 14.7929C11.5251 17.111 13.6781 18.4033 15.2802 19.121C16.6787 19.7475 18.3296 19.2562 19.6167 17.9691L19.9114 17.6744L16.3752 15.4241C15.7026 16.4048 14.4319 16.979 13.1632 16.4434C12.2017 16.0376 10.9302 15.3445 9.7929 14.2071C8.65557 13.0698 7.96243 11.7983 7.55659 10.8369C7.02105 9.56817 7.59528 8.29741 8.57591 7.62479L6.32562 4.08863L6.0309 4.38335C4.74381 5.67044 4.25256 7.32131 4.87905 8.71986C5.59671 10.322 6.88908 12.4749 9.20711 14.7929ZM14.4626 20.9462C12.6479 20.1334 10.2905 18.7047 7.7929 16.2071C5.29532 13.7096 3.86668 11.3521 3.05381 9.53748C1.9781 7.13609 2.95955 4.62627 4.61669 2.96913L4.91141 2.67441C5.81615 1.76967 7.32602 1.93541 8.01294 3.01488L10.8437 7.46315C10.9957 7.70201 11.0393 7.99411 10.9637 8.26696C10.8881 8.53981 10.7005 8.76784 10.4472 8.89446L9.81354 9.2113C9.38171 9.42721 9.2931 9.80786 9.39916 10.0591C9.73804 10.8619 10.3046 11.8904 11.2071 12.7929C12.1097 13.6955 13.1381 14.262 13.9409 14.6009C14.1922 14.7069 14.5728 14.6183 14.7887 14.1865L15.1056 13.5528C15.2322 13.2996 15.4602 13.1119 15.7331 13.0363C16.0059 12.9607 16.298 13.0044 16.5369 13.1564L20.9852 15.9871C22.0646 16.674 22.2304 18.1839 21.3256 19.0886L21.0309 19.3833C19.3738 21.0405 16.8639 22.0219 14.4626 20.9462Z" fill="#008000"/>
+                        </svg>+212 11 392732</li>
+                    <li class="flex text-justify"><svg fill="#008000" width="23px" height="23px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><title>Gmail icon</title><path d="M24 4.5v15c0 .85-.65 1.5-1.5 1.5H21V7.387l-9 6.463-9-6.463V21H1.5C.649 21 0 20.35 0 19.5v-15c0-.425.162-.8.431-1.068C.7 3.16 1.076 3 1.5 3H2l10 7.25L22 3h.5c.425 0 .8.162 1.069.432.27.268.431.643.431 1.068z"/></svg>Contact@PetPlanet.com</li>
+                 </ul>
+                 <div class="h-96 sm:w-3/4 w-full border-2 shadow border-black my-10">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15507.232597711677!2d-9.237964572144926!3d32.28875268785696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xdae38215289d04f%3A0x4f1a417ca4734e4!2sSafi%2046000%2C%20Morocco!5e0!3m2!1sen!2sus!4v1645738072378!5m2!1sen!2sus" width="100%" height="100%" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>             
+                </div>
+            </div>
        <footer-component></footer-component>
     </div>
   </template>
