c40248b5ec82d5541a8f25636c4930b07a28126d
unknown <zaaferkenza08@gmail.com>
2023-03-31 11:18:12 +0000

    New Products section

--- petplanet/src/components/HelloWorld.vue
+++ petplanet/src/components/HelloWorld.vue
@@ -2,15 +2,60 @@
   <div>
      <header-component></header-component>
      <div class="flex w-full items-center justify-between">
-         <div class="flex flex-col justify-center items-center "> 
-            <h5 class="font-extrabold text-5xl text-green-700 pl-8 w-full">Where your pets' happiness comes first - Shop with us!</h5>
+         <div class="flex flex-col sm:justify-center items-center "> 
+            <h5 class="sm:mt-0 mt-8 font-extrabold sm:text-5xl text-4xl text-green-700 pl-8 w-full ">Where your pets' happiness comes first - Shop with us!</h5>
             <p class="text-sm p-8 text-justify">Welcome to PetPlanet, your online destination for high-quality pet products. From food and treats to toys and accessories, we offer a wide range of items to keep your pets healthy and happy. Shop now and experience the convenience of fast shipping and exceptional customer service.</p>
             <button type="button" class="text-green-700 bg-white border-solid border-2 border-green-700 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center inline-flex items-center">
                 Discover
                 <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1 text-green-700 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
             </button>
          </div>
-         <img src="../assets/bg.png" class="w-1/2"/>
+         <img src="../assets/bg.png" class="hidden sm:flex w-1/2"/>
+     </div>
+     <div class="w-full h-screen"> 
+         <p class="text-center text-2xl m-10">New products</p>
+         <div class="flex flex-col sm:flex-row justify-between items-center m-2"> 
+              <div class="flex flex-col justify-around items-center"> 
+                 <img src="../assets/image.png"/>
+                 <p>Home Divine Martin Sellier</p>
+                 <p>799,00د.م.Price</p>
+              </div>
+              <div class="flex flex-col justify-around items-center"> 
+                 <img src="../assets/image(1).png"/>
+                 <p>Dog treat dispenser - Martin Sellier</p>
+                 <p>299,00د.م.Price</p>
+              </div>
+              <div class="flex flex-col justify-around items-center"> 
+                 <img src="../assets/image(2).png"/>
+                 <p>Martin Sellier Black Cricket Basket</p>
+                 <p>349,00د.م.Price</p>
+              </div>
+              <img class="sm:block hidden" src="../assets/Rectangle.png"/>
+         </div>
+         <div class="flex flex-col sm:flex-row justify-between items-center m-2"> 
+          <img class="sm:block hidden" src="../assets/Rectangle8.png"/>
+              <div class="flex flex-col justify-around items-center"> 
+                 <img src="../assets/image7.png"/>
+                 <p>Aboistop Electro - Barking control collar</p>
+                 <p>1.199,00د.م.Price</p>
+              </div>
+              <div class="flex flex-col justify-around items-center"> 
+                 <img src="../assets/image7(1).png"/>
+                 <p>Ownat Grain Free Prime Adult Chicken & Turkey 12kg</p>
+                 <p>580,00د.م.Price</p>
+              </div>
+              <div class="flex flex-col justify-around items-center"> 
+                 <img src="../assets/image7(2).png"/>
+                 <p>Trixie folding metal cage S / M-L</p>
+                 <p>990,00د.م.Price</p>
+              </div>
+         </div>
+     </div>
+     <div class="w-full"> 
+      <p class="text-center text-2xl m-10">Shop By Pet</p>
+      <div class="flex flex-wrap gap-4"> 
+         <img class="w-32 h-32 rounded-full border-2 border-green-700" src=""/> 
+      </div>
      </div>
   </div>
 </template>
--- petplanet/src/components/inc/HeaderComponent.vue
+++ petplanet/src/components/inc/HeaderComponent.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <div class="bg-white text-green-700 font-bold w-full h-10 flex justify-center items-center "> 
+        <div class="bg-white text-green-700 font-bold w-full h-10 hidden sm:flex justify-center items-center "> 
             <p>Get 50% off on selected items | shop now</p>
         </div>
-        <div class="bg-green-700 w-full shadow-lg h-16 flex justify-between items-center">
+        <div class="hidden bg-green-700 w-full shadow-lg h-16 sm:flex justify-between items-center">
             <div class="ml-8  w-1/4 text-white text-sm font-medium flex justify-around items-center"> 
                 <a>Home</a>
                 <a>Shop</a>
@@ -11,27 +11,74 @@
                 <a>Blog</a>
             </div>
              <img src="./../../assets/whitelogo.png" class="w-16 h-12"/>
-             <div class="w-1/3 text-white flex justify-between items-center"> 
-                <div class="flex w-1/3 justify-center items-center"> 
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 101 101" width="30" height="30">
-                    <path d="M63.3 59.9c3.8-4.6 6.2-10.5 6.2-17 0-14.6-11.9-26.5-26.5-26.5S16.5 28.3 16.5 42.9 28.4 69.4 43 69.4c6.4 0 12.4-2.3 17-6.2l20.6 20.6c.5.5 1.1.7 1.7.7.6 0 1.2-.2 1.7-.7.9-.9.9-2.5 0-3.4L63.3 59.9zm-20.4 4.7c-12 0-21.7-9.7-21.7-21.7s9.7-21.7 21.7-21.7 21.7 9.7 21.7 21.7-9.7 21.7-21.7 21.7z" fill="white"/>
-                </svg> 
-                <input placeholder="search..."
-                :class="['placeholder-white', 'mx-2', 'bg-transparent', 'w-16', 'border-none', {'w-32': isExpanded}]"
-                class="focus:outline-none"
-                @focus="isExpanded = true"
-                @blur="isExpanded = false"
-                />
+             <div class="w-1/4 text-white flex justify-around items-center"> 
+                <div class="flex w-1/2 justify-center p-2 items-center"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><path fill="white" fill-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm3-12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 7a7.489 7.489 0 0 1 6-3 7.489 7.489 0 0 1 6 3 7.489 7.489 0 0 1-6 3 7.489 7.489 0 0 1-6-3Z" clip-rule="evenodd"/></svg><p>Se connecter</p></div>
+                <div class="mr-8 dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                    <span class="badge badge-sm indicator-item">8</span>
+                    </div>
+                </label>
+                <div tabindex="0" class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+                    <div class="card-body">
+                    <span class="font-bold text-lg text-green-700">8 Items</span>
+                    <span class="text-green-700 font-light text-md">Subtotal: $999</span>
+                    <div class="card-actions">
+                        <button class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">View cart</button>
+                    </div>
+                    </div>
                 </div>
-                <div class="flex w-1/3 justify-between items-center"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><path fill="white" fill-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm3-12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 7a7.489 7.489 0 0 1 6-3 7.489 7.489 0 0 1 6 3 7.489 7.489 0 0 1-6 3 7.489 7.489 0 0 1-6-3Z" clip-rule="evenodd"/></svg><p>Se connecter</p></div>
-                <div class="flex w-1/3 justify-center items-center relative">
-                <svg xmlns="http://www.w3.org/2000/svg" id="Layer_1" x="0" y="0" version="1.1" width="24" height="24" viewBox="0 0 52 52" xml:space="preserve">
-                    <path d="m43.51 32.165 6.44-19.17a1 1 0 0 0-.14-.9.986.986 0 0 0-.81-.41H12.74l-1.29-5.21c-.47-1.66-2-2.82-3.72-2.82H3c-.55 0-1 .44-1 1 0 .55.45 1 1 1h4.73c.83 0 1.57.56 1.78 1.33l7.99 32.18a4.696 4.696 0 0 0-3.32 4.49c0 2.58 2.1 4.69 4.69 4.69 2.58 0 4.68-2.11 4.68-4.69 0-1-.31-1.93-.84-2.69h15.88c-.54.76-.85 1.69-.85 2.69 0 2.58 2.1 4.69 4.68 4.69 2.59 0 4.69-2.11 4.69-4.69 0-2.59-2.1-4.69-4.69-4.69-.04 0-.09 0-.13.01-.02-.01-.04-.01-.06-.01H19.51l-1.52-6.11h24.57c.43 0 .81-.28.95-.69z" fill="white"/>
-                </svg>
                 </div>
              </div>
         </div>
+        <div class="sm:hidden flex justify-between items-center bg-green-700"> 
+            <div class="text-white w-1/4 flex items-center"><img src="./../../assets/whitelogo.png" class="ml-4 mr-1 w-12 h-8"/>PetPlanet</div>
+            <div class="w-1/3 text-white flex justify-between items-center">
+                 
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><path fill="white" fill-rule="evenodd" d="M12 22c5.523 0 10-4.477 10-10S17.523 2 12 2 2 6.477 2 12s4.477 10 10 10Zm3-12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm-9 7a7.489 7.489 0 0 1 6-3 7.489 7.489 0 0 1 6 3 7.489 7.489 0 0 1-6 3 7.489 7.489 0 0 1-6-3Z" clip-rule="evenodd"/></svg>
+                <div class="dropdown dropdown-end">
+                <label tabindex="0" class="btn btn-ghost btn-circle">
+                    <div class="indicator">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
+                    <span class="badge badge-sm indicator-item">8</span>
+                    </div>
+                </label>
+                <div tabindex="0" class="card card-compact dropdown-content w-52 bg-base-100 shadow">
+                    <div class="card-body">
+                    <span class="font-bold text-lg text-green-700">8 Items</span>
+                    <span class="text-green-700 font-light text-md">Subtotal: $999</span>
+                    <div class="card-actions">
+                        <button class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2">View cart</button>
+                    </div>
+                    </div>
+                </div>
+               </div>
+               <button id="dropdownHoverButton" data-dropdown-toggle="dropdownHover" data-dropdown-trigger="hover" class="mr-4" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7">
+                    <svg width="25px" height="25px" viewBox="0 0 24 24" fill="#FFFFFF" xmlns="http://www.w3.org/2000/svg"> 
+                        <path id="Subtract" fill-rule="evenodd" clip-rule="evenodd" d="M7.25 1.25C3.93629 1.25 1.25 3.93629 1.25 7.25V16.75C1.25 20.0637 3.93629 22.75 7.25 22.75H16.75C20.0637 22.75 22.75 20.0637 22.75 16.75V7.25C22.75 3.93629 20.0637 1.25 16.75 1.25H7.25ZM4.84615 7C4.51691 7 4.25 7.33579 4.25 7.75C4.25 8.16421 4.51691 8.5 4.84615 8.5H19.1538C19.4831 8.5 19.75 8.16421 19.75 7.75C19.75 7.33579 19.4831 7 19.1538 7H4.84615ZM4.25 12.25C4.25 11.8358 4.51691 11.5 4.84615 11.5H19.1538C19.4831 11.5 19.75 11.8358 19.75 12.25C19.75 12.6642 19.4831 13 19.1538 13H4.84615C4.51691 13 4.25 12.6642 4.25 12.25ZM4.84615 16C4.51691 16 4.25 16.3358 4.25 16.75C4.25 17.1642 4.51691 17.5 4.84615 17.5H19.1538C19.4831 17.5 19.75 17.1642 19.75 16.75C19.75 16.3358 19.4831 16 19.1538 16H4.84615Z" fill="#FFFFFF"/>
+                    </svg>
+                </button> 
+
+             </div>
+        </div>
     </div>
+    <div id="dropdownHover" class="z-10 hidden bg-white divide-y divide-green-100 rounded-lg shadow w-44 ">
+                    <ul class="py-2 text-sm text-green-700 " aria-labelledby="dropdownHoverButton">
+                    <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-green-600 hover:text-white">Home</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-green-600 hover:text-white">Shop</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-green-600 hover:text-white">Blog</a>
+                    </li>
+                    <li>
+                        <a href="#" class="block px-4 py-2 hover:bg-green-600 hover:text-white">Contact</a>
+                    </li>
+                    </ul>
+                </div>
 </template>
 <script>
 export default {
