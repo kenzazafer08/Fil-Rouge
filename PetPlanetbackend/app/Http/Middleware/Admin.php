e35b1c7d1f6e7f9b67294a13038bfa4c0353864f
unknown <zaaferkenza08@gmail.com>
2023-04-04 11:26:11 +0000

    Get Users

--- PetPlanetbackend/app/Http/Middleware/Admin.php
+++ PetPlanetbackend/app/Http/Middleware/Admin.php
@@ -4,15 +4,14 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Admin
 {
     public function handle(Request $request, Closure $next)
     {
         if ($request->user() && $request->user()->role !== '1') {
-            return response()->json(['message' => 'Unauthorized'], 401);
-        }
-
-        return $next($request);
+        return response()->json(['message' => 'Unauthorized'], 401);
+       } else  return $next($request);
     }
 }
--- PetPlanetbackend/routes/api.php
+++ PetPlanetbackend/routes/api.php
@@ -16,6 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/register', [App\Http\Controllers\AuthController::class, 'register']);
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'This is a test endpoint']);
-});
+Route::get('/Users', [App\Http\Controllers\UserController::class, 'index'])->middleware('admin');
