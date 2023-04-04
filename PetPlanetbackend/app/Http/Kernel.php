c1140acf94e6c089589b3dd0acd51673c5030750
unknown <zaaferkenza08@gmail.com>
2023-04-04 10:58:07 +0000

    Authentication middleware

--- PetPlanetbackend/app/Http/Kernel.php
+++ PetPlanetbackend/app/Http/Kernel.php
@@ -53,6 +53,8 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $middlewareAliases = [
+        'admin' => \App\Http\Middleware\Admin::class,
+        'author' => \App\Http\Middleware\Author::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
