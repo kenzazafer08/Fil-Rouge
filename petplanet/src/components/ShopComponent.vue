6a739b5e2d0d229b39ca51a24e64d224ef34043f
unknown <zaaferkenza08@gmail.com>
2023-04-14 15:01:45 +0000

    Shop page

--- petplanet/src/components/ShopComponent.vue
+++ petplanet/src/components/ShopComponent.vue
@@ -18,35 +18,11 @@
                 <fieldset class="p-2">
                   <legend class="sr-only">Checkbox variants</legend>
 
-                  <div class="flex items-center mb-4">
+                  <div v-for="item in pets" :key="item.id" class="flex items-center mb-4">
                       <input id="checkbox-1" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" >
-                      <label for="checkbox-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Cat</label>
+                      <label for="checkbox-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ item.name }}</label>
                   </div>
 
-                  <div class="flex items-center mb-4">
-                      <input id="checkbox-2" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                      <label for="checkbox-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Dog</label>
-                  </div>
-
-                  <div class="flex items-center mb-4">
-                      <input id="checkbox-3" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                      <label for="checkbox-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Fish</label>
-                  </div>
-                  
-                  <div class="flex items-center mb-4">
-                      <input id="checkbox-3" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                      <label for="checkbox-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Reptile</label>
-                  </div>
-
-                  <div class="flex items-center mb-4">
-                      <input id="checkbox-3" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                      <label for="checkbox-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Bird</label>
-                  </div>
-
-                  <div class="flex items-center mb-4">
-                      <input id="checkbox-3" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                      <label for="checkbox-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Small pet</label>
-                  </div>
                 </fieldset>
               </div>
               <div>
@@ -54,20 +30,11 @@
                 <fieldset class="p-2">
                   <legend class="sr-only">Checkbox variants</legend>
 
-                  <div class="flex items-center mb-4">
+                  <div v-for="item in categories" :key="item.id" class="flex items-center mb-4">
                       <input id="checkbox-1" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" >
-                      <label for="checkbox-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Food</label>
+                      <label for="checkbox-1" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">{{ item.name }}</label>
                   </div>
 
-                  <div class="flex items-center mb-4">
-                      <input id="checkbox-2" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                      <label for="checkbox-2" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Wellness</label>
-                  </div>
-
-                  <div class="flex items-center mb-4">
-                      <input id="checkbox-3" type="checkbox" value="" class="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 rounded focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600">
-                      <label for="checkbox-3" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Accesorries</label>
-                  </div>
                 </fieldset>
               </div>
               <div> 
@@ -93,30 +60,9 @@
               </div>
              </div>
              <div class="text-center w-[75%] sm:mr-10 mr-0 grid sm:grid-cols-3 grid-cols-1 gap-8 p-0 sm:items-center items-place-center">
-              <div class="flex flex-col justify-around items-center"> 
-                 <img src="../assets/image7.png"/>
-                 <p>Aboistop Electro - Barking control collar<br/>1.199,00د.م.Price</p>
-                 <p></p>
-              </div>
-              <div class="flex flex-col justify-around items-center"> 
-                 <img src="../assets/image7(1).png"/>
-                 <p>Ownat Grain Free Prime Adult Chicken & Turkey 12kg<br/>580,00د.م.Price</p>
-              </div>
-              <div class="flex flex-col justify-around items-center"> 
-                 <img src="../assets/image7(2).png"/>
-                 <p>Trixie folding metal cage S / M-L<br/>990,00د.م.Price</p>
-              </div>
-              <div class="flex flex-col justify-around items-center"> 
-                 <img src="../assets/image.png"/>
-                 <p>Home Divine Martin Sellier<br/>799,00د.م.Price</p>
-              </div>
-              <div class="flex flex-col justify-around items-center"> 
-                 <img src="../assets/image(1).png"/>
-                 <p>Dog treat dispenser - Martin Sellier<br/>299,00د.م.Price</p>
-              </div>
-              <div class="flex flex-col justify-around items-center"> 
-                 <img src="../assets/image(2).png"/>
-                 <p>Martin Sellier Black Cricket Basket<br/>349,00د.م.Price</p>
+              <div v-for="item in products" :key="item.id" class="flex flex-col justify-around items-center"> 
+                 <img :src="image(item.image)"/>
+                 <p>{{ item.name }}<br/>{{ item.price}}د.م.Price</p>
               </div>
              </div>
         </div>
@@ -126,6 +72,7 @@
   </template>
   
   <script>
+  import axios from 'axios';
   import HeaderComponent from './inc/HeaderComponent.vue';
   import FooterComponent from './inc/FooterComponent.vue';
   export default {
@@ -133,8 +80,59 @@
     components :{HeaderComponent,FooterComponent},
     data() {
       return {
+        categories : [],
+        pets : [],
+        products : [],
       }
-    }
+    },
+    mounted(){
+    this.getCategories()
+    this.getPets()
+    this.getProducts()
+   },
+   methods : {
+    getCategories(){
+      console.log('test')
+      axios
+        .get("http://127.0.0.1:8000/api/categories")
+        .then((response) => {
+          // set the authenticated state to true
+          this.categories = response.data.categories;
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+          // handle error response
+        });
+    },
+    getPets(){
+      console.log('test')
+      axios
+        .get("http://127.0.0.1:8000/api/pets")
+        .then((response) => {
+          // set the authenticated state to true
+          this.pets = response.data.pets;
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+          // handle error response
+        });
+    },
+    getProducts(){
+      axios
+        .get("http://127.0.0.1:8000/api/random")
+        .then((response) => {
+          // set the authenticated state to true
+          this.products = response.data.products;
+        })
+        .catch((error) => {
+          console.log(error.response.data);
+          // handle error response
+        });
+    },
+    image(name){
+      return 'http://127.0.0.1:8000/api/images/'+name;
+   }
+  }
   }
   </script>
   
