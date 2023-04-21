611ff45167e3209eac3c98b619b4635c9d54cdf3
unknown <zaaferkenza08@gmail.com>
2023-04-21 15:51:28 +0000

    some changes

--- petplanet/src/components/ProfileComponent.vue
+++ petplanet/src/components/ProfileComponent.vue
@@ -6,10 +6,43 @@
 </template>
 
 <script>
+const token = localStorage.getItem("token");
+import axios from 'axios'
 import FooterComponent from './inc/FooterComponent.vue'
 import HeaderComponent from './inc/HeaderComponent.vue'
 export default {
   components: { HeaderComponent, FooterComponent },
+  data(){
+    return{
+        user : {},
+        orders : [],
+    }
+  },
+  mounted(){
+    this.getuser()
+    this.getorders()
+  },
+  methods : {
+    getuser(){
+        axios.get('http://127.0.0.1:8000/api/user/', {
+          headers: {
+            Authorization: `Bearer ${token}` // include the token in the headers of the API request
+          }
+        })
+        .then(response => { // set the authenticated state to true
+          this.user = response.data; 
+          console.log(this.user)// set the user data
+        })
+        .catch(error => {
+          console.log(error.response.data);
+          // handle error response
+        });
+    },
+
+    getorders(){
+        console.log('hi')
+    }
+  }
 }
 </script>
 
